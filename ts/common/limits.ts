aef9ca0e2a5dc694838f058c1eae16d64565e2a3
Atlee Brink <atlee@atleebrink.com>
2024-03-19 13:03:54 -0700

    increased maximum kernel radius allowed by the controls

--- ts/common/limits.ts
+++ ts/common/limits.ts
@@ -2,4 +2,4 @@
 
 // Constants to set operational limits.
 
-export const MAX_KERNEL_RADIUS = 5 // kernel size is (radius + 1 + radius)^2
+export const MAX_KERNEL_RADIUS = 9 // kernel size is (radius + 1 + radius)^2
